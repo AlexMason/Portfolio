0441802ff31f1dbc2124aae5ca1cf61ca5483c07
Alexander Mason <tantrex93@gmail.com>
2021-03-21 18:16:37 -0400

    Finished Nav + Added headed hero

---
+++ script.js
@@ -0,0 +1,43 @@
+//Nav anchoring + scroll START
+
+let nav = document.getElementById("nav");
+let navLinks = document.querySelectorAll("#typing-links > a");
+
+const resizeObserver = new ResizeObserver((entries) => {
+  console.log("hi");
+});
+
+resizeObserver.observe(nav);
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY <= 70) {
+    nav.classList.remove("pt-3");
+    nav.classList.remove("pb-3");
+    nav.classList.add("pt-5");
+    nav.classList.add("pb-5");
+  } else {
+    nav.classList.remove("pt-5");
+    nav.classList.remove("pb-5");
+    nav.classList.add("pt-3");
+    nav.classList.add("pb-3");
+  }
+});
+
+navLinks.forEach((navLink) => {
+  navLink.onclick = (e) => {
+    e.preventDefault();
+
+    let href = navLink.attributes.href.nodeValue;
+
+    navLinks.forEach((n) => n.classList.remove("active"));
+    navLink.classList.add("active");
+
+    if (href !== "" && href.substr(0, 1) === "#") {
+      let scrollToElement = document.getElementById(href.substr(1));
+      scrollToElement.scrollIntoView();
+    } else if (href !== "") {
+      window.open(href, "_blank");
+    }
+  };
+});
+//Nav anchoring + scroll END
--- typing.js
+++ typing.js
@@ -1,24 +1,12 @@
-function sleep(ms) {
-  return new Promise((r) => setTimeout(r, ms));
-}
-
 window.addEventListener("load", async (event) => {
   let name = document.querySelector("[id=typing-name]");
-  let nameDefaultText = name.innerText;
+  let nameDefaultText = name.innerHTML;
 
   setInterval(() => {
     if (name.innerText.includes("_")) {
-      name.innerText = nameDefaultText;
+      name.innerHTML = nameDefaultText;
     } else {
-      name.innerText = nameDefaultText + "_";
+      name.innerHTML = nameDefaultText + "_";
     }
   }, 750);
-
-  //links
-  //rebuild links
-  let linksElement = document.querySelector("[id=typing-links]");
-  let reg = linksElement.innerText.match(/([A-Z])\w+/g);
-  let template = "{ Links [] }";
-
-  console.log(linksElement.innerHTML);
 });
