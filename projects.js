0ef4a55479f6162c475f5b213e07c0d42cc96393
Alexander Mason <alex@alexmason.me>
2021-05-28 13:51:10 -0400

    added red badge project

--- projects.js
+++ projects.js
@@ -30,42 +30,37 @@ let projects = [
   {
     title: "Static Layout",
     img: "./assets/kast.png",
-    desc:
-      "This is a visual recreation of the Kastapp.co website. It is meant to demonstrate the understanding of CSS properties and layout.",
+    desc: "This is a visual recreation of the Kastapp.co website. It is meant to demonstrate the understanding of CSS properties and layout.",
     url: "https://alexmason.github.io/StaticLayout/",
   },
   {
     title: "CSS Creature",
     img: "./assets/csscreature.png",
-    desc:
-      "This is a Pokemon creature created entirely by using CSS and HTML. It features an animated moving eye.",
+    desc: "This is a Pokemon creature created entirely by using CSS and HTML. It features an animated moving eye.",
     url: "https://codepen.io/AlexMason/pen/zYomjGL",
   },
   {
     title: "F1 Racing API",
     img: "./assets/F1RacingApp.png",
-    desc:
-      "A small API project testing the usage of Fetch and ability to manipulate and update the DOM.",
+    desc: "A small API project testing the usage of Fetch and ability to manipulate and update the DOM.",
     url: "https://alexmason.github.io/API-Project-1/",
   },
   {
     title: "NASA APOD API",
     img: "./assets/nasaapi.png",
-    desc:
-      "A project implementing the NASA Astronomy Picture of the Day API to provide an interactive explorer for current and previous photos.",
+    desc: "A project implementing the NASA Astronomy Picture of the Day API to provide an interactive explorer for current and previous photos.",
     url: "https://wd80api2-59733.web.app/",
   },
   {
     title: "FlickView",
     img: "./assets/bluebadge.png",
-    desc:
-      "FlickView is a movie review + watchlist app that I built with a team of 3 people.",
+    desc: "FlickView is a movie review + watchlist app that I built with a team of 3 people.",
     url: "https://aem-bb-client.herokuapp.com/",
   },
   {
-    title: "Red Badge",
-    img: "https://via.placeholder.com/450?text=Red%20Badge",
-    desc: "This is a placeholder for my red badge project.",
+    title: "upstring",
+    img: "./assets/redbadge.png",
+    desc: "upstring if a complete topic and comment issue board.  It was developed with TypeScript + React techologies and utilizes tailwindcss for the design.",
     url: "",
   },
 ];
